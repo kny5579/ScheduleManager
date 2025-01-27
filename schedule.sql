39efb2d2e2a952a5623520d9832a3aeba2faef29
나연 <kny5579@naver.com>
2025-01-27 13:53:11 +0900

    feat: 작성자 테이블 생성 sql (lv3)

--- schedule.sql
+++ schedule.sql
@@ -1,3 +1,12 @@
+CREATE TABLE author
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    created_date DATETIME,
+    updated_date DATETIME
+);
+
 CREATE TABLE schedule
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -5,5 +14,8 @@ CREATE TABLE schedule
     password VARCHAR(20) NOT NULL,
     contents VARCHAR(1000),
     created_date DATETIME,
-    updated_date DATETIME
+    updated_date DATETIME,
+    author_id BIGINT,
+    foreign key (author_id) references author(id)
 );
+
